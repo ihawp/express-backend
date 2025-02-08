4a115361cf4b14d096cce787e2cf5b894c076429
ihawp <ihawpebay@gmail.com>
2025-02-08 13:28:11 -0600

    Blog enpoint set up... will set up endpoints on /projects and /api/projects... will refactor apps to use ihawp.com/api/projects

--- public/javascripts/pug-blog.js
+++ public/javascripts/pug-blog.js
@@ -1,5 +1,16 @@
-const l = await fetch('https://ihawp.com/projects', {
-    headers: '',
-    method: 'GET',
-});
+async function getData(url) {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
 
+getData('https://ihawp.com/projects')
+    .then(response => console.log(response));
--- routes/blog.js
+++ routes/blog.js
@@ -2,9 +2,28 @@ const express = require('express');
 
 const router = express.Router();
 
+const mysql = require('mysql2');
+
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'blog',
+    port: 3306
+};
+
+const pool = mysql.createPool(dbConfig);
+
 router.get('/api', (req, res) => {
-    let cool = {wow: 'wow'};
-    res.send(cool);
+    const query = 'SELECT * FROM post ORDER BY timestamp DESC';
+
+    pool.query(query, (error, results) => {
+        if (error) {
+            console.error('Error fetching data:', error);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        res.json(results);
+    });
 });
 router.get('/', (req, res) => {
     res.render('blog', { title: 'bananaphone blog'});
