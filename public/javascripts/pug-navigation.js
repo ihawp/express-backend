073dffab3d1e6fe81e99f8452c94d5addeed6386
ihawp <ihawpebay@gmail.com>
2025-02-09 21:16:12 -0600

    mobile navigation... react site does not have one worthwhile, although I believe if someone was accessing that site on their phone all they would want to do is look at my links, anywho adding it here... alongside a popup contact form for desktop?

--- public/javascripts/pug-navigation.js
+++ public/javascripts/pug-navigation.js
@@ -1,2 +1,13 @@
-let nav = document.getElementById('mobile-nav');
+// ihawp
+// functional mobile navigation with classes
 
+let mobileNavButton = document.getElementById('open-mobile-nav');
+let mobileNav = document.getElementById('mobile-nav');
+
+mobileNavButton.addEventListener('click', () => {
+    if (mobileNav.classList.contains('display-sm')) {
+        mobileNav.classList.remove('display-sm');
+    } else {
+        mobileNav.classList.add('display-sm');
+    }
+});
