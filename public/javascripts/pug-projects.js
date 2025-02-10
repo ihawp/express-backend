8cc55f4d47295f7c179b7fb11b0ea2d8f047fbac
ihawp <ihawpebay@gmail.com>
2025-02-09 23:19:59 -0600

    good night

--- public/javascripts/pug-projects.js
+++ public/javascripts/pug-projects.js
@@ -1,14 +1,11 @@
 const l = document.querySelectorAll('div[id^="project-"]');
-
 let open = l[l.length - 1];
 
-l.forEach((item) => {
-    item.addEventListener('click', (event) => {
-
-        item.scrollIntoView(true);
 
 
-        event.target === item ? console.log(true) : console.log(false);
+l.forEach((item, key) => {
+    item.addEventListener('click', () => {
+        item.scrollIntoView({ block: "center", behavior: "auto" });
         if (open !== item) {
             item.lastElementChild.classList.remove('display');
             open.lastElementChild.classList.add('display');
